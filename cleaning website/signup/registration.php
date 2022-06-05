df041864f2d7ea945c54547fce00f3756b8cdbb9
Dayanand Gawade <95270686+Daya3611@users.noreply.github.com>
2022-06-05 13:01:07 +0530

    1

---
+++ cleaning website/signup/registration.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h3>book appointment</h3>
+            <input type="text" placeholder="your name" class="box" name="name" id="yourname"required autocomplete="off">
+            <input type="number" placeholder="your number" class="box" name="number" id="number" required autocomplete="off">
+            <input type="email" placeholder="your email" class="box" name="email" id="email" required autocomplete="off">
+            <input type="text" placeholder="your address" class="box" name="address" id="address" required autocomplete="off">
+            <input type="submit" value="book now" class="btn" required>
+        </form>
+
+    </div>
+</body>
+</html>
